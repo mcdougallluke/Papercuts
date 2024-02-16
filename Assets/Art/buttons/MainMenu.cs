7c847cd667f21167fdecd3a36b7993226fa9f48c
Luke <lmcdougall@unm.edu>
2024-02-15 22:04:34 -0700

    linked test scene for friday

--- Assets/Art/buttons/MainMenu.cs
+++ Assets/Art/buttons/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-    SceneManager.LoadSceneAsync(2);
+    SceneManager.LoadSceneAsync(5);
     }
     
     public void QuitGame()
